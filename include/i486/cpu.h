8d80141ec07b23fbcbcf6c5304bbc3941d97a1b0
DipeshChouhan <dcdon077@gmail.com>
2025-06-08 23:14:53 +0530

    control and test registers are added to cpu.h

--- include/i486/cpu.h
+++ include/i486/cpu.h
@@ -16,6 +16,13 @@ typedef struct Cpu {
 
   // instruction pointer
   uint32_t eip;
+
+  // control registers
+  uint32_t cr0, cr1, cr2, cr3;
+
+  // debug registers
+  uint32_t dr0, dr1, dr2, dr3, dr4, dr5, dr6, dr7;
+  // memory management registers
 } Cpu;
 
 #endif
