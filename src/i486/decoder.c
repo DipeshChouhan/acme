3c137688731a11adb51d9a84ec69c01e7aa5c59e
DipeshChouhan <dcdon077@gmail.com>
2025-11-08 19:24:36 +0530

    cleaned decoder.c file

--- src/i486/decoder.c
+++ src/i486/decoder.c
@@ -9,22 +9,7 @@ extern int two_byte_opcodemap[16][16];
 
 extern int group_opcodemap[8][8];
 
-// NOTE: I am assuming all instructions in a single contigous memory for now
-// and for testing of decoder I am creating a array containing all instructions
-// for now
-
-// TODO: get instructions 
-static uint8_t *instructions = NULL;
-static int eip = 0; // temporary eip
-
-// a temporary init for decoder
-void init_decoder(uint8_t *instructions, int size) {
-
-  // initialize instructions
-}
-
-
-// TODO: implement decoder []
+// TODO: implement decoder no testing required for now it's simple enough
 
 AcmeStatus decode(Cpu *cpu, Instruction *instruction) {
 
